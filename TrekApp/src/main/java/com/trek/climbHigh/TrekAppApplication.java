7ecf626d5465b14ea70188b400a5eaa2f1f28afc
Harsha <Harsha@DESKTOP-SCEQ7CP>
2023-01-23 17:53:25 +0530

    New Repo

---
+++ TrekApp/src/main/java/com/trek/climbHigh/TrekAppApplication.java
@@ -0,0 +1,13 @@
+package com.trek.climbHigh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrekAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrekAppApplication.class, args);
+	}
+
+}
---
+++ TrekApp/src/test/java/com/trek/climbHigh/TrekAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trek.climbHigh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrekAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
